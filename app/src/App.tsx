ddd17595188bf9a3057f26119cae2ea2057a8940
sabi1125 <sabir_barahi4@hotmail.com>
2022-08-13 19:08:07 +0900

    navbar added

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Navigator from './routes/Navigator';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Navigator />
+       Top page
     </div>
   );
 }
