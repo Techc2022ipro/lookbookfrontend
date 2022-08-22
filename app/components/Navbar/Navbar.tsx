5d5e88ace8bea3ec452861a274b814064b6e447d
sabi1125 <sabir_barahi4@hotmail.com>
2022-08-22 13:50:35 +0900

    wip

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import NavRouter from "../../routes/NavRouter";
+import Searchbar from "../SearchBar/Searchbar";
+
+const Navbar = () => {
+  return (
+    <Router>      
+      <div className="navbar">
+      <h2 className="logo">Lookbook</h2>
+      <ul className="navLinks">
+        <Searchbar />
+        <li className="navLink">
+          <Link to = "/">Top</Link>
+        </li>
+
+        <li className="navLink">
+          <Link to = "/explore">Explore</Link>
+        </li>
+
+        <li className="navLink">
+          <Link to = "/products">Products</Link>
+        </li>
+
+        <li className="navLink">
+          <Link to = "/login">Login</Link>
+        </li>
+      </ul>
+    </div>
+      <NavRouter />
+    </Router>
+  )
+}
+
+export default Navbar;
