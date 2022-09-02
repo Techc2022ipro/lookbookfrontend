3c6e912c5e03157202c2ecf1e9bcf4521818438f
sabi1125 <sabir_barahi4@hotmail.com>
2022-09-02 23:52:00 +0900

    handle Submit: resolves #17

--- app/src/pages/Login/Login.tsx
+++ app/src/pages/Login/Login.tsx
@@ -1,9 +1,11 @@
+import axios from "axios";
 import React, {useState} from "react";
 
 const Login = () => {
   // also have to make a signup page
   const [identifier, setIdentifier] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+
   const handleIdentifier = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIdentifier(e.target.value);
   }
@@ -12,8 +14,16 @@ const Login = () => {
     setPassword(e.target.value);
   }
 
+  const postData = async () => {
+    await axios.post("http://localhost:8000/login", {
+      email: identifier,
+      password: password
+    });
+  }
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    postData().then(res => { console.log(res) });
   }
 
   return (
