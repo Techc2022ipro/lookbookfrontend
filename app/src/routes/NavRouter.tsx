7429d30cd2dac4bf30fbe440ca9ee8ae962a7a22
sabi1125 <sabir_barahi4@hotmail.com>
2022-09-13 12:36:41 +0900

    deleted explore

--- app/src/routes/NavRouter.tsx
+++ app/src/routes/NavRouter.tsx
@@ -2,14 +2,12 @@ import {Route, Switch} from "react-router-dom"
 import Explore from "../pages/Explore/Explore";
 import Feeds from "../pages/Feeds/Feeds";
 import Login from "../pages/Login/Login";
-import Products from "../pages/Products/Products";
 
 const NavRouter = () => {
   return (
     <Switch>
       <Route path="/" exact component={Explore} />
       <Route path="/feeds" component={Feeds} />
-      <Route path="/products" component={Products} />
       <Route path="/login" component={Login} />
     </Switch>
   )
