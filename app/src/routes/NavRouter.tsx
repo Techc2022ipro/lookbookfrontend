65eb35cd7fbaf9d6517477bb0067bd3ac490868c
Lugo-Lugo <hikaru.ujigawa@gmail.com>
2022-09-14 15:39:07 +0900

    signup wip

--- app/src/routes/NavRouter.tsx
+++ app/src/routes/NavRouter.tsx
@@ -2,6 +2,7 @@ import {Route, Switch} from "react-router-dom"
 import Explore from "../pages/Explore/Explore";
 import Feeds from "../pages/Feeds/Feeds";
 import Login from "../pages/Login/Login";
+import Signup from "../pages/Signup/Signup";
 
 const NavRouter = () => {
   return (
@@ -9,6 +10,7 @@ const NavRouter = () => {
       <Route path="/" exact component={Explore} />
       <Route path="/feeds" component={Feeds} />
       <Route path="/login" component={Login} />
+      <Route path="/Signup" component={Signup} />
     </Switch>
   )
 } 
