f5d9b0fcbbd4c74d7821891bab209da8eb879e07
sabi1125 <sabir_barahi4@hotmail.com>
2022-09-21 05:23:55 +0900

    fixed bugs

--- app/src/Requests/Requests.ts
+++ app/src/Requests/Requests.ts
@@ -6,5 +6,6 @@ const Requests = {
   get: (url:string) => axios.get(url).then(responseBody),
   getWithCredentials: (url:string) => axios.get(url, { withCredentials: true }).then(responseBody),
   post: (url:string, body: {}) => axios.post(url, body, {withCredentials: true}).then(responseBody),
+  auth: () => axios.get("http://localhost:8000/isVerified", { withCredentials: true }).then(responseBody),
 }
 export default Requests;
